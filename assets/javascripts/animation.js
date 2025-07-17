f448de536b3b208aa294a628880616590d194c5f
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-07-17 14:28:20 +0700

    init

--- assets/javascripts/animation.js
+++ assets/javascripts/animation.js
@@ -1,5 +1,5 @@
 // image animation
-const tl = gsap.timeline();
+let tl = gsap.timeline();
 
 tl.from(".hero-img", {
     duration: 1.5,
@@ -8,25 +8,11 @@ tl.from(".hero-img", {
     scale: 0
 });
 
-
-gsap.timeline({
-  scrollTrigger: {
-    trigger: ".box",
-    start: "top 80%",
-    toggleActions: "play none none none",
-    snap: {
-      snapTo: 'labels',
-      duration: { min: 0.2, max: 3 },
-      delay: 0.2,
-      ease: 'power1.inOut'
-    }
-  }
-})
-.from(".box", {
-  opacity: 0,
-  y: 100,
-  scale: 0.7,
-  filter: "blur(20px)",
-  ease: "power2.out",
-  duration: 1.2
-});
+  ScrollReveal().reveal('.box', {
+    duration: 1000,
+    distance: '200px',
+    origin: 'bottom',
+    opacity: 0,
+    easing: 'ease-out',
+    reset: false
+  });
